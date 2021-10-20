1a08fe535c52057d4b6e289c5a679b986b962092
Kristen Scheel <kristen.scheel@gmail.com>
2021-10-20 14:15:05 -0400

    updated progress bar colors

--- scroll.js
+++ scroll.js
@@ -1,5 +1,8 @@
+const pixelsTag = document.querySelector("div.pixels")
 const bodyTag = document.querySelector("body")
 const progressTag = document.querySelector("div.progress")
+const sections = document.querySelectorAll("section")
+const circleTag = document.querySelector("div.circle")
 
 
 //when we scroll the page, make a progress bar that keeps track of the distance
@@ -11,4 +14,17 @@ document.addEventListener("scroll", function () {
   const percentage = pixels / totalScrollableDistance
 
   progressTag.style.width = `${100 * percentage}%`
+})
+
+// when we scroll the page, see how far down we've scrolled
+//for each section, check to see if we have passed it 
+// if we have, then update the left Nav indicator circle
+document.addEventListener("scroll", function () {
+  const pixels = window.pageYOffset
+
+  sections.forEach(section => {
+    if (section.offsetTop - 100 <= pixels) {
+      circleTag.style.backgroundColor = section.getAttribute("data-sect")
+    }
+  })
 })
